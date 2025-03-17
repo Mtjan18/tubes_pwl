ab48ed2ede1aa9bbcf0442492615ed33b792adbb
Mtjan18 <marchellinotjandra18@gmail.com>
2025-03-17 13:49:48 +0700

    tubes-1-layouting-login

---
+++ resources/views/pages/example.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Example Page</h1>
+    <p>This is an example page that demonstrates how to create a new page in the Laravel application.</p>
+</div>
+@endsection
